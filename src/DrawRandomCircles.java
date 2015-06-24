592e1aeeb155c397fc39fb97e943b20e4f69a215
Edgar Pino <edgarbeltran@me.com>
2015-06-24 00:31:31 -0500

    Created method to draw random circles.

--- src/DrawRandomCircles.java
+++ src/DrawRandomCircles.java
@@ -1,5 +1,20 @@
 /**
  * Created by edgar971 on 6/22/15.
  */
+import javax.swing.JFrame;
+
 public class DrawRandomCircles {
+
+   public static void main(String[] args) {
+       JFrame frame = new JFrame("Random Circles");
+       RandomCircles panel = new RandomCircles();
+
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+       frame.getContentPane().add(panel);
+       frame.pack();
+       frame.setVisible(true);
+   }
+
+
 }
--- src/RandomCircles.java
+++ src/RandomCircles.java
@@ -7,23 +7,44 @@ import java.util.Random;
  */
 public class RandomCircles extends JPanel{
     //private variables
-    private final int NUM_CIRCLES = 10, MAX_X = 350, MAX_Y = 350, MAX_RAD = 50;
+    private final int NUM_CIRCLES = 10, MAX_WIDTH = 400, MAX_RAD = 200;
     private Random generator;
 
     //constructor
     public RandomCircles() {
-        generator = new Random();
 
+        generator = new Random();
         setBackground(Color.gray);
-        setPreferredSize(new Dimension(500, 500));
+        setPreferredSize(new Dimension(MAX_WIDTH, MAX_WIDTH));
 
     }
 
     public void paintComponent(Graphics page) {
+        int radius, diameter, x = 0, y = 0;
         super.paintComponent(page);
-        int radious;
+        page.setColor(Color.white);
 
+        for (int counter = 0; counter < NUM_CIRCLES; counter++) {
+            //generate random radius
+            radius = generator.nextInt(MAX_RAD) + 1;
+            //calculate diameter
+            diameter = radius * 2;
+            //set x and y to center circle
+            x = MAX_WIDTH / 2 - radius;
+            y = MAX_WIDTH / 2- radius;
+            //change colors
+            if(counter%2 == 0) {
+                page.setColor(Color.blue);
+            } else {
+                page.setColor(Color.green);
+            }
+
+            //create oval
+            page.fillOval(x,y, diameter, diameter);
+
+        }
     }
+
 
 
 
